a5a433afbbd53d3c866177d8a11a948bc81a9087
Matthew Watters <watters91@gmail.com>
2014-04-27 20:32:29 +1000

    First function created, still to be tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matthew Watters - 27 April 2014
+##
+## The following functions will allow creation of a custom matrix object that 
+## allows caching of its inverse, rahter than re-calculating it every time it is
+## needed.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Creates a new custom matrix object which is able to cache its inverse.
 
+makeCacheMatrix <- function(storedMatrix = matrix()) {
+    # initialise inversed matrix
+    inversedMatrix <- NULL
+    
+    # set stored matrix to new value and re-initialise the cached inverse
+    set <- function(newMatrix) {
+        storedMatrix <<- newMatrix
+        inversedMatrix <<- NULL
+    }
+    
+    # return stored matrix
+    get <- function() storedMatrix
+    
+    # set the inverse of the stored matrix
+    setInverse <- function(inverse) inversedMatrix <<- inverse
+    
+    # return inverse of the stored matrix
+    getInverse <- function() inversedMatrix
+    
+    # methods within makeCacheMatrix
+    list(set = set
+       , get = get
+       , setInverse = setInverse
+       , getInverse = getInverse)
 }
 
 
